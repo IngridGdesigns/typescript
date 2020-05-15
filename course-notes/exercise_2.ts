4b47899f5dec144fce0820fa9fa52b30b97c6635
IngridGdesigns <aleja.morada@gmail.com>
2020-05-14 18:04:40 -0700

    Array and object example

--- course-notes/exercise_2.ts
+++ course-notes/exercise_2.ts
@@ -9,3 +9,24 @@ const flower = {
 };
 
 console.log(flower.name);
+
+// example 2
+const student = {
+  name: 'Morgan',
+  age: 26,
+  hobbies: ['Drawing', 'Cooking', 'Travelling'],
+};
+
+console.log(student.name);
+console.log(student.hobbies);
+
+let favoriteActivities: string[]; // tells type that it's an array of strings
+favoriteActivities = ['Swimming', 'Dancing', 'Watching Movies'];
+console.log(favoriteActivities);
+
+for (const hobby of student.hobbies) {
+  // <--- this is where we see Typescript inference really shine!!
+  console.log(hobby);
+  console.log(hobby.toLowerCase);
+  //knows for certainty that hobby will be a string because of the types defined in student
+}
